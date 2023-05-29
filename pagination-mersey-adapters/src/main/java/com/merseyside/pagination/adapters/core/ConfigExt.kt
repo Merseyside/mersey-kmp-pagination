f62a62c8b1c99a5f6c9f2e4e42569afed838d9b5
Ivan Sablin <ivansablin@MacBook-Pro-Ivan.local>
2023-05-29 17:30:24 +0600

    Add ext for mersey-adapters

---
+++ pagination-mersey-adapters/src/main/java/com/merseyside/pagination/adapters/core/ConfigExt.kt
@@ -0,0 +1,20 @@
+package com.merseyside.pagination.adapters.core
+
+import com.merseyside.adapters.core.feature.pagination.Config
+import com.merseyside.adapters.core.model.VM
+import com.merseyside.merseyLib.kotlin.entity.result.filterSuccessValues
+import com.merseyside.pagination.contract.PaginationContract
+import com.merseyside.pagination.contract.TwoWayPaginationContract
+
+fun <Data, Parent, Model : VM<Parent>> Config<Parent, Model>.setPagination(
+    paginationContract: PaginationContract<List<Data>>
+) {
+    onNextPage = paginationContract.onPageResultFlow.filterSuccessValues()
+}
+
+fun <Data, Parent, Model : VM<Parent>> Config<Parent, Model>.setPagination(
+    paginationContract: TwoWayPaginationContract<List<Data>>
+) {
+    onNextPage = paginationContract.onPageResultFlow.filterSuccessValues()
+    onPrevPage = paginationContract.onPrevPageResultFlow.filterSuccessValues()
+}
