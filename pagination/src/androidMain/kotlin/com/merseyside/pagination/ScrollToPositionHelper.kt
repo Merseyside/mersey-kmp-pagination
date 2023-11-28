d3ea0daf334e51aa655606c5e972895b647edf4c
Merseyside <ivanklessablin@gmail.com>
2023-11-28 11:22:16 +0600

    Add debug logs

--- pagination/src/androidMain/kotlin/com/merseyside/pagination/ScrollToPositionHelper.kt
+++ pagination/src/androidMain/kotlin/com/merseyside/pagination/ScrollToPositionHelper.kt
@@ -3,11 +3,19 @@ package com.merseyside.pagination
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
+import com.merseyside.merseyLib.kotlin.logger.ILogger
+import com.merseyside.merseyLib.kotlin.logger.Logger
 
-class ScrollToPositionHelper(private val recyclerView: RecyclerView) {
+class ScrollToPositionHelper(private val recyclerView: RecyclerView): ILogger {
 
     fun setPosition(position: Int) {
-        val adapter = requireNotNull(recyclerView.adapter)
+        val adapter = recyclerView.adapter
+
+        if (adapter == null) {
+            Logger.logInfo(tag, "Adapter hasn't been set! Position restoring is impossible.")
+            return
+        }
+
         if (adapter.itemCount != 0) {
             scroll(position)
         } else {
@@ -31,4 +39,7 @@ class ScrollToPositionHelper(private val recyclerView: RecyclerView) {
             else -> throw UnsupportedOperationException()
         }
     }
+
+    override val tag: String
+        get() = "ScrollToPositionHelper"
 }
